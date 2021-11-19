b5ff9fb736d1664db7244baa23eae623fdb818e5
andrewsimmons <neon.duncan@gmail.com>
2021-11-19 09:04:25 -0600

    solved the ruby recursive

--- ruby/recursive_search.rb
+++ ruby/recursive_search.rb
@@ -1,5 +1,12 @@
 def recursive_search(arr, target)
-  # type your code in here
+  if arr.last == target
+    return true
+  elsif arr.last
+    arr.pop
+    recursive_search(arr, target)
+  else
+    return false
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,8 +18,16 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: false"
   puts "=>", recursive_search([3, 2, 1], 4)
 
+  puts
+
+  puts "Expecting: false"
+  puts "=>", recursive_search([], 4)
+
   # Don't forget to add your own!
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+# The function takes the arr and compares the last element to the target, if that is false,
+# It checks to see if arr.last exist, if so it removes the last element and calls itself with the
+# recently changed array and the same target, if the last element does not exist it returns false.
